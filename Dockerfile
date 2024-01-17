bf735649ed90793d81657d57740116f040d8ff87
Md Sadique Inam <sadique@wiseboxs.com>
2024-01-17 18:10:41 +0530

    Implement automatic configuration flow
    This update implements an automated configuration generation flow in the start-up script. It processes variables prefixed with 'PDNS_' and accordingly creates or adjusts the configuration. Additionally, GitHub workflows for auto-release, build-testing, and Docker publishing have been established. A variety of .gitignore files and necessary configurations have been added as well.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 ARG IMAGE=alpine:latest
 
 FROM $IMAGE AS base
-RUN apk --no-cache add dumb-init
+RUN apk --no-cache add dumb-init grep
 RUN apk add --upgrade pdns pdns-backend-pgsql
 
 COPY ./start.sh /usr/local/bin/start.sh
+COPY ./pdns.conf /usr/local/bin/pdns.conf
 RUN chmod +x /usr/local/bin/start.sh
 
 #ENTRYPOINT ["dumb-init", "--"]
--- start.sh
+++ start.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-GPGSQL_HOST=${GPGSQL_HOST:-host.docker.internal}
-GPGSQL_PORT=${GPGSQL_PORT:-5432}
-GPGSQL_DB=${GPGSQL_DB:-pdns_db}
-GPGSQL_USER=${GPGSQL_USER:-postgres}
-GPGSQL_PASS=${GPGSQL_PASS:-postgres}
-GPGSQL_DNSSEC=${GPGSQL_DNNSEC:-yes}
-GPGSQL_EXTRA_CONNECTION_PARAMETERS=${GPGSQL_EXTRA_CONNECTION_PARAMETERS:-""}
-GPGSQL_PREPARED_STATEMENTS=${GPGSQL_PREPARED_STATEMENTS:-yes}
-PDNS_API_KEY=${PDNS_API_KEY:-changeme}
 
-cat << EOF > /etc/pdns/pdns.conf
+createConf() {
+  property_names=$(grep -oP "[0-9a-z-_]*=" /usr/local/bin/pdns.conf | awk -F= '{print $1}' | tr '\n' ' ')
+
+  for name in $property_names
+  do
+    # Converting name to upper case and replace "-" with "_" and also remove all space and #
+    var_name=$(echo "$name" | tr 'a-z-' 'A-Z_')
+
+    # Check if the environment variable with the var_name is set
+    var_value=$(eval echo \$"$var_name")
+    if [ -n "$var_value" ]; then
+      # Replace the value in the pdns.conf file and stripped_name should start from the beginning of the line
+      sed -i "s/.*$name=.*/$name=$var_value/g" /etc/pdns/pdns.conf
+    else
+      echo "$var_name environment variable not available."
+    fi
+  done
+}
+
+concatConf() {
+  GPGSQL_HOST=${GPGSQL_HOST:-localhost}
+  GPGSQL_PORT=${GPGSQL_PORT:-5432}
+  GPGSQL_DB=${GPGSQL_DB:-pdns_db}
+  GPGSQL_USER=${GPGSQL_USER:-postgres}
+  GPGSQL_PASS=${GPGSQL_PASS:-postgres}
+  GPGSQL_DNSSEC=${GPGSQL_DNNSEC:-yes}
+  GPGSQL_EXTRA_CONNECTION_PARAMETERS=${GPGSQL_EXTRA_CONNECTION_PARAMETERS:-""}
+  GPGSQL_PREPARED_STATEMENTS=${GPGSQL_PREPARED_STATEMENTS:-yes}
+  PDNS_API_KEY=${PDNS_API_KEY:-changeme}
+
+  cat << EOF >> /etc/pdns/pdns.conf
+
 # psql Configuration
 #
 # Launch gpgsql backend
@@ -24,5 +46,9 @@ gpgsql-dnssec=${GPGSQL_DNSSEC}
 gpgsql-extra-connection-parameters=${GPGSQL_EXTRA_CONNECTION_PARAMETERS}
 gpgsql-prepared-statements=${GPGSQL_PREPARED_STATEMENTS}
 EOF
+}
 
-exec /usr/sbin/pdns_server --daemon=no --guardian=no --loglevel=9 --log-dns-details=on --webserver=yes --api=yes --api-key="${PDNS_API_KEY}"
+createConf
+concatConf
+
+exec /usr/sbin/pdns_server --daemon=no --guardian=no --loglevel=9 --log-dns-details=on
